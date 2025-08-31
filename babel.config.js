478cab6fd680c423ece2e014be6ac5bec345ad9e
Vansh <chauhanvansh350@gmail.com>
2025-08-31 21:50:55 +0530

    Disable console statements in release variant

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = function(api) {
+  const isProduction = api.env('production');
+
+  const presets = ['module:metro-react-native-babel-preset'];
+  const plugins = [];
+
+  if (isProduction) {
+    // remove console in production
+    plugins.push('transform-remove-console');
+  }
+
+  return {
+    presets,
+    plugins,
+  };
+};
--- index.ts
+++ index.ts
@@ -1,4 +1,10 @@
 import { registerRootComponent } from 'expo';
 import App from './App';
 
+if (!__DEV__) {
+  console.log = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+}
+
 registerRootComponent(App);
