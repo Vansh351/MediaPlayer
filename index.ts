da88b27779d95a4c2f8343017919a4f770cfa02c
Vansh <chauhanvansh350@gmail.com>
2025-08-31 23:46:41 +0530

    Remove babel config from project

--- index.ts
+++ index.ts
@@ -1,10 +1,4 @@
 import { registerRootComponent } from 'expo';
 import App from './App';
 
-if (!__DEV__) {
-  console.log = () => {};
-  console.warn = () => {};
-  console.error = () => {};
-}
-
 registerRootComponent(App);
