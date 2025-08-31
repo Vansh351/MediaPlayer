b55a4207c1026c6209c7a1d69a69786ee7c0e4ae
Vansh <chauhanvansh350@gmail.com>
2025-08-31 22:14:40 +0530

    Add babel plugin for private methods

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,11 @@ module.exports = function(api) {
   const isProduction = api.env('production');
 
   const presets = ['module:metro-react-native-babel-preset'];
-  const plugins = [];
+  
+  // Add the new plugin to the plugins array
+  const plugins = [
+    ['@babel/plugin-transform-private-methods', { 'loose': true }]
+  ];
 
   if (isProduction) {
     // remove console in production
